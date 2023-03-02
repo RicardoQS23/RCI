bf9d64f6ef0865124c7fe538cb185f75969df847
Ricardo <ricardo.silva@junitec.pt>
2023-03-02 20:50:35 +0000

    [LineCommand] consumes different types of line commands

--- project.c
+++ project.c
@@ -1,3 +1,4 @@
+//#include "project.h"
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -13,8 +14,59 @@
 #include <errno.h>
 #include <signal.h>
 
-int main(void)
+enum commands{
+    JOIN,
+    DJOIN,
+    CREATE,
+    DELETE,
+    GET,
+    LEAVE,
+    EXIT
+};
+
+int compare_cmd(char *line_cmd)
 {
-    //Start coding!
+    char cmds[7][7] = {"join", "djoin", "create", "delete", "get", "leave", "exit"};
+    for (int i = 0; i < 7; i++)
+    {
+        if(strcmp(line_cmd,cmds[i]) == 0)
+            return i;
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[])
+{
+    enum commands cmd;
+    int cmd_code = 0;
+    //char net[4], id[3], bootid[3], bootIP[15], bootTCP[6]; 
+    /* Command line treatment*/
+    cmd_code = compare_cmd(argv[1]);
+    switch(cmd_code){
+        case 0:
+            cmd = JOIN;
+            break;
+        case 1:
+            cmd = DJOIN;
+            break;
+        case 2:
+            cmd = CREATE;
+            break;
+        case 3:
+            cmd = DELETE;
+            break;
+        case 4:
+            cmd = GET;
+            break;
+        case 5:
+            cmd = LEAVE;
+            break;
+        case 6:
+            cmd = EXIT;
+            break;
+        default:
+            printf("Unknown Command!\n");
+            exit(1);
+    }
     return 0;
 }
